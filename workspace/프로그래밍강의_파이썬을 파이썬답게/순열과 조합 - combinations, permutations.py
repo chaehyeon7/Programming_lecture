31bd1361d9ce0bc228676f21b46e295cffa9e86e
chaehyeon8 <81363193+chaehyeon7@users.noreply.github.com>
2022-04-13 14:53:09 +0900

    파이썬을 파이썬답게 순열과 조합 - combinations, permutations 정리

---
+++ workspace/프로그래밍강의_파이썬을 파이썬답게/순열과 조합 - combinations, permutations.py
@@ -0,0 +1,25 @@
+def permute(arr):
+    result = [arr[:]]
+    c = [0] * len(arr)
+    i = 0
+    while i < len(arr):
+        if c[i] < i:
+            if i % 2 == 0:
+                arr[0], arr[i] = arr[i], arr[0]
+            else:
+                arr[c[i]], arr[i] = arr[i], arr[c[i]]
+            result.append(arr[:])
+            c[i] += 1
+            i = 0
+        else:
+            c[i] = 0
+            i += 1
+    return result
+
+# itertools.permutation를 이용하면, for문을 사용하지 않고도 순열을 구할 수 있다.
+
+import itertools
+
+pool = ['A', 'B', 'C']
+print(list(map(''.join, itertools.permutations(pool)))) # 3개의 원소로 수열 만들기
+print(list(map(''.join, itertools.permutations(pool, 2)))) # 2개의 원소로 수열 만들기
